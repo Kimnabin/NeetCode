d853d706b0d60315b484437a8c66e5ee3945bb9b
부콩득 <ducvucong01@gmail.com>
2025-07-26 01:50:45 +0900

    day2 :: ValidAnagram

--- neetcode/src/ValidAnagram.java
+++ neetcode/src/ValidAnagram.java
@@ -1,3 +1,5 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// This solution uses HashTable to count the occurrences of each character in both strings.
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
@@ -30,5 +32,42 @@ public class main {
 
         System.out.println(solution.isAnagram(s1, t1)); // true
         System.out.println(solution.isAnagram(s2, t2)); // false
+    }
+}
+
+// This solution uses hashmap to count the occurrences of each character in both strings.
+
+class Solution2 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            retufn false;
+        }
+
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for ( int i = 0; i < s.lenth(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return countS.equals(countT);
+    }
+}
+
+// This solution uses sorting to check if two strings are anagrams.
+import java.util.Arrays;
+class Solution3 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
     }
 }
